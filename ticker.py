73fb9a22fca2b873c4cfdf1e61513a69ec1a6968
kyeung7 <kevin.yeung.1@uvm.edu>
2020-05-24 10:41:19 -0400

    Working tkinter scrolling text window

--- ticker.py
+++ ticker.py
@@ -1,6 +1,14 @@
 # Kevin Yeung
 # Scrolling stock ticker
 
+# need to install yahoo_fin
+# need to install requests_html (need to restart after)
+# need to install tkinter
+
+import tkinter as tk
+import tkinter.font as tkFont
+from tkinter import Frame, Entry
+from yahoo_fin import stock_info as market
 #################################### HISTORICAL ##############################
 ##
 ### may need to install yfinance (via pip)
@@ -24,12 +32,37 @@
 ##print(stockData.info)
 
 #################################### LIVE ##############################
-# need to install yahoo_fin
-# need to install requests_html (need to restart after)
-from yahoo_fin import stock_info as market
 
 stock = "AAPL"
 stockPrice = round(market.get_live_price(stock), 2) #formatted to 2 decimals by rounding
 
 #live price of aapl stock
 print("Current Price of: " + stock + " is $" + str(stockPrice))
+
+#################################### SCROLL TEST ##############################
+
+
+def setText():
+    setText.msg = setText.msg[1:] + setText.msg[0]
+    svar.set(setText.msg)
+    root.after(delay, setText)
+    
+root = tk.Tk() # root is base of window
+root.iconbitmap("stock.ico") #sets window icon
+root.title("Live stock ticker")
+root.geometry("750x30") #fixed window of tkinter root
+#root.overrideredirect(1) #no border but also no exit
+
+#root.attributes("-alpha", 0.0)
+root.wm_attributes("-topmost", 1) #set to top most window (windows os)
+
+delay = 200 # character delay in milliseconds
+svar = tk.StringVar() # used to edit widget text
+
+fontStyle = tkFont.Font(family="Lucida Grande", size=11)
+label = tk.Label(root, textvariable = svar, height = 30, width=200, font=fontStyle, borderwidth=2, relief="groove") # creates label given params
+
+setText.msg = "Example text here eeeeeeeeeeeeeeeeeeeeiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiieeeeeeee "
+setText()
+label.pack()
+root.mainloop()
